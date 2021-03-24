f2c46826bc5b438f1a3d8dc27b9aadb6df8ebb8d
Po <s5819874@hotmail.com>
2021-03-24 09:53:49 +0800

    feat: add create todo feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -5,6 +5,20 @@ const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
 
+//create
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/todos', (req, res) => {
+  const name = req.body.name
+  const UserId = req.user.id
+  Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(err => res.send(err))
+})
+
+//read
 router.get('/:id', (req, res) => {
   const id = req.params.id
   const UserId = req.user.id
