39b6266035d54b423bd25869e285e05dc235261e
Po <s5819874@hotmail.com>
2021-03-24 10:48:22 +0800

    feat: add update todo feature and fix read feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -10,7 +10,7 @@ router.get('/new', (req, res) => {
   res.render('new')
 })
 
-router.post('/todos', (req, res) => {
+router.post('/', (req, res) => {
   const name = req.body.name
   const UserId = req.user.id
   Todo.create({ name, UserId })
@@ -22,11 +22,34 @@ router.post('/todos', (req, res) => {
 router.get('/:id', (req, res) => {
   const id = req.params.id
   const UserId = req.user.id
-  User.findByPk(UserId)
-    .then(user => {
-      return Todo.findOne({ UserId, id })
-    })
+  return Todo.findOne({ where: { UserId, id } })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(err => res.send(err))
+})
+
+//update
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  const UserId = req.user.id
+  Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      res.render('edit', { todo: todo.toJSON() })
+    })
+    .catch(err => res.send(err))
+})
+
+router.put('/:id', (req, res) => {
+  const userId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+
+  Todo.findOne({ where: { userId, id } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(err => res.send(err))
 })
 
