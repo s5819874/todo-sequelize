73df4bf4eb92747172ed198705e5d6f21026199b
Po <s5819874@hotmail.com>
2021-03-24 14:17:57 +0800

    feat: add showing partial messages on login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -55,7 +55,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 //logout
