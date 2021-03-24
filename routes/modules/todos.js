c0ce9228aa4882821f22aacb7a3f0e092a76b144
Po <s5819874@hotmail.com>
2021-03-24 09:43:48 +0800

    feat: add relations between User and Tdo models in home and todos routers

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,10 +3,15 @@ const router = express.Router()
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 router.get('/:id', (req, res) => {
   const id = req.params.id
-  return Todo.findByPk(id)
+  const UserId = req.user.id
+  User.findByPk(UserId)
+    .then(user => {
+      return Todo.findOne({ UserId, id })
+    })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(err => res.send(err))
 })
